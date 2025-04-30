428750e9af6ccf7eb042d54c28f6f10fd64997ea
Richard <richardjonhshm@gmail.com>
2025-04-30 17:30:43 -0300

    Tirando a flag carencia

--- resources/views/cotacao/cotacao2.blade.php
+++ resources/views/cotacao/cotacao2.blade.php
@@ -155,7 +155,7 @@ text-white focus:outline-none bg-gray-500 bg-opacity-10 rounded-lg border-2
 
     <tr>
         <td rowspan="2" style="border-color: #FFF;" class="text-center text-sm border-2 border-r border-b text-white dark:text-white" style="vertical-align:middle;">Faixa Etária</td>
-        <td colspan="2" style="border-color: #FFF;" class="text-center text-sm border-2 border-r border-b text-white dark:text-white">Com Copar</td>
+        <td colspan="2" style="border-color: #FFF;" class="text-center text-sm border-2 border-r border-b text-white dark:text-white">Com Coparticipação</td>
 
     </tr>
     <tr>
@@ -258,22 +258,22 @@ focus:ring-4 focus:ring-gray-200 bg-red-400">
 
 
 <div class="flex justify-around items-center w-full mt-4 py-2">
-    <label for="status_carencia">
-        <input type="checkbox" style="border-color: #bde521;" checked name="status_carencia" id="status_carencia" class="w-6 h-6 text-teal-600 bg-white border-2 rounded">
-        <span class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-2xl text-white">Com Carências</span>
-    </label>
+{{--    <label for="status_carencia">--}}
+{{--        <input type="checkbox" style="border-color: #bde521;" checked name="status_carencia" id="status_carencia" class="w-6 h-6 text-teal-600 bg-white border-2 rounded">--}}
+{{--        <span class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-2xl text-white">Com Carências</span>--}}
+{{--    </label>--}}
 
-    @if($status_desconto)
-        <label for="status_desconto">
-            <input type="checkbox" name="status_desconto" id="status_desconto" class="w-6 h-6 text-teal-600 bg-white border border-2 border-black rounded dark:bg-white dark:border-purple-900">
-            <span class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-2xl text-white">Desconto</span>
-        </label>
-    @endif
-    <div>
-        <button class="btn_ambulatorial focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-            Ambulatorial
-        </button>
-    </div>
+{{--    @if($status_desconto)--}}
+{{--        <label for="status_desconto">--}}
+{{--            <input type="checkbox" name="status_desconto" id="status_desconto" class="w-6 h-6 text-teal-600 bg-white border border-2 border-black rounded dark:bg-white dark:border-purple-900">--}}
+{{--            <span class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300 text-2xl text-white">Desconto</span>--}}
+{{--        </label>--}}
+{{--    @endif--}}
+{{--    <div>--}}
+{{--        <button class="btn_ambulatorial focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">--}}
+{{--            Ambulatorial--}}
+{{--        </button>--}}
+{{--    </div>--}}
 
 </div>
 
